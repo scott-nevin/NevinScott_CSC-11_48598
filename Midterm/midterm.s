d6f4d0b3adc1f90d088faab1c76eca81fba6156a
scott-nevin <scott.nevin@att.net>
2014-10-22 12:30:52 -0700

    Signed-off-by: scott-nevin <scott.nevin@att.net>

--- Midterm/midterm.s
+++ Midterm/midterm.s
@@ -8,15 +8,18 @@ main:
 	push {lr}
 	sub sp, sp, #4
 	
-	ldr r0, addr_messin
+	ldr r0, addr_menu		@ display menu
+	bl printf	
+	
+	ldr r0, addr_messin		@ prompt for menu choice
 	bl printf
 	
-	ldr R0, addr_format
+	ldr R0, addr_format		@ collect menu choice
 	mov r1, sp
 	bl scanf
 	
 	ldr r1, [sp]
-	ldr r0, addr_messout
+	ldr r0, addr_messout	@ Display result, send to function
 	bl printf
 	
 	add sp, sp, #4
@@ -24,13 +27,15 @@ main:
 	
 _exit:
 	mov pc, lr
-	
+
+addr_menu: .word menu	
 addr_messin: .word messagein
 addr_format: .word scanformat
 addr_messout: .word messageout
 
 .data
-messagein: .asciz "Enter your number: "
+menu: .asciz "CSC-11 Midterm:/n1. Problem 1\n2. Problem 2\n3. Problem 3"
+messagein: .asciz "Enter your choice: "
 scanformat: .asciz "%d"
 messageout: .asciz "Your number was %d\n"
 		
