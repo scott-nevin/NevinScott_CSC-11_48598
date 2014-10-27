1b2a06ce6f21cf962d91306555e0713804fc7c9e
scott-nevin <scott.nevin@att.net>
2014-10-27 02:21:21 -0700

    Signed-off-by: scott-nevin <scott.nevin@att.net>

--- Midterm/midterm.s
+++ Midterm/midterm.s
@@ -21,12 +21,10 @@ main:
 	ldr r1, [sp]
 	cmp r1, #1 				@ send to problem1
 	beq _prob1
-	cmp r1, #2				@ send to problem 2
+	@cmp r1, #2				@ send to problem 2
 	beq _prob2
-	cmp r1, #3				@ send to problem 3
+	@cmp r1, #3				@ send to problem 3
 	beq _prob3
-	cmp r1, #4
-	bx lr
 		
 	add sp, sp, #4
 	pop {pc}
