04748b0f8f3fba490f507f6fb8143ffedefdf442
scott-nevin <scott.nevin@att.net>
2014-10-27 00:30:20 -0700

    Signed-off-by: scott-nevin <scott.nevin@att.net>

--- Midterm/midterm.s
+++ Midterm/midterm.s
@@ -29,13 +29,14 @@ main:
 	beq _prob2
 	cmp r1, #3				@ send to problem 3
 	beq _prob3
-	@cmp r1, #4				@ send to exit
-	@beq _exit
+	cmp r1, #4				@ send to exit
+	beq _exit
 	
 	add sp, sp, #4
 	pop {pc}
 	
 _exit:
+	mov r0, #7
 	mov pc, lr
 	swi 0
 
