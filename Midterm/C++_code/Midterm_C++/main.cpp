f6d2c367c518569ffa160cbc70069049243ebc58
scott-nevin <scott.nevin@att.net>
2014-10-27 19:17:07 -0700

    Signed-off-by: scott-nevin <scott.nevin@att.net>

--- Midterm/C++_code/Midterm_C++/main.cpp
+++ Midterm/C++_code/Midterm_C++/main.cpp
@@ -7,7 +7,7 @@
 //Libraries
 #include <cstdlib>
 #include <iostream>
-using namespace std;
+//using namespace std;
 
 //Global variables and constants
 
@@ -25,48 +25,48 @@ int main(int argc, char** argv) {
     //begin loop for program
     do{
     //Display menu and send user to appropriate function
-    cout<<"CSC-11 Midterm"<<endl;
-    cout<<"Please select a problem"<<endl;
-    cout<<"1. Problem 1"<<endl;
-    cout<<"2. Problem 2"<<endl;
-    cout<<"3. Problem 3"<<endl;
-    cout<<"type anything to exit program"<<endl;
-    cin>>choice;
+    std::cout<<"CSC-11 Midterm"<<std::endl;
+    std::cout<<"Please select a problem"<<std::endl;
+    std::cout<<"1. Problem 1"<<std::endl;
+    std::cout<<"2. Problem 2"<<std::endl;
+    std::cout<<"3. Problem 3"<<std::endl;
+   std::cout<<"type anything to exit program"<<std::endl;
+    std::cin>>choice;
     
     if(choice=='1'){
         prob1();
-        cout<<"\n"<<endl;
+        std::cout<<"\n"<<std::endl;
     }
     else if(choice=='2'){
         prob2();
-        cout<<"\n"<<endl;
+        std::cout<<"\n"<<std::endl;
     }
     else if(choice=='3'){
         prob3();
-        cout<<"\n"<<endl;
+        std::cout<<"\n"<<std::endl;
     }
     }while(choice>='1'&&choice<='3');
     
-    cout<<"\nYou typed "<<choice<<" to exit the program. "<<endl;
-    cout<<"Have a nice day."<<endl;
+    std::cout<<"\nYou typed "<<choice<<" to exit the program. "<<std::endl;
+    std::cout<<"Have a nice day."<<std::endl;
     return 0;
 }
 
 void prob1(){
-    cout<<"\nProblem 1:"<<endl;
+    std::cout<<"\nProblem 1:"<<std::endl;
     
     //Declare variables
     int hours, rate, gross=0;
     
     //prompt user for number of hours worked
-    cout<<"Input number of hours worked \n to determine gross pay for the week"<<endl;
-    cout<<"Maximum hours = 60"<<endl;
-    cout<<"hours: ";
-    cin>>hours;
+    std::cout<<"Input number of hours worked \n to determine gross pay for the week"<<std::endl;
+    std::cout<<"Maximum hours = 60"<<std::endl;
+    std::cout<<"hours: ";
+    std::cin>>hours;
     //prompt for pay rate
-    cout<<"Input pay rate in dollars per hour"<<endl;
-    cout<<"rate: ";
-    cin>>rate;
+    std::cout<<"Input pay rate in dollars per hour"<<std::endl;
+    std::cout<<"rate: ";
+    std::cin>>rate;
     
     //determine how much straight, double, and triple time
     if(hours>40){
@@ -82,8 +82,8 @@ void prob1(){
     }
     
     //output results
-    cout<<"Hours worked = "<<hours<<" hours"<<endl;
-    cout<<"Gross pay = $"<<gross<<".00"<<endl; 
+    std::cout<<"Hours worked = "<<hours<<" hours"<<std::endl;
+    std::cout<<"Gross pay = $"<<gross<<".00"<<std::endl; 
 }
 void prob2(){
     
@@ -92,20 +92,20 @@ void prob2(){
     int hours, total=0, hrsRslt;
     
     //prompt user to choose plan
-    cout<<"Please choose a plan:"<<endl;
-    cout<<"a) $30 per month, 11 hours access. Additional hours"<<endl;
-    cout<<"   are $3 up to 22 hours then $6 for all additional hours. "<<endl;
-    cout<<"b) $35 per month, 22 hours access. Additional hours"<<endl;
-    cout<<"   are $2 up to 44 hours then $4 for all additional hours. "<<endl;
-    cout<<"c) $40 per month, 33 hours access. Additional hours "<<endl;
-    cout<<"   are $1 up to 66 hours then $2 for all additional hours. "<<endl;
-    cout<<"plan choice: ";
-    cin>>choice;
+    std::cout<<"Please choose a plan:"<<std::endl;
+    std::cout<<"a) $30 per month, 11 hours access. Additional hours"<<std::endl;
+    std::cout<<"   are $3 up to 22 hours then $6 for all additional hours. "<<std::endl;
+    std::cout<<"b) $35 per month, 22 hours access. Additional hours"<<std::endl;
+    std::cout<<"   are $2 up to 44 hours then $4 for all additional hours. "<<std::endl;
+    std::cout<<"c) $40 per month, 33 hours access. Additional hours "<<std::endl;
+    std::cout<<"   are $1 up to 66 hours then $2 for all additional hours. "<<std::endl;
+    std::cout<<"plan choice: ";
+    std::cin>>choice;
     
     //prompt for hours
-    cout<<"Input number of hours"<<endl;
-    cout<<"you intend to use every month: ";
-    cin>>hours;
+    std::cout<<"Input number of hours"<<std::endl;
+    std::cout<<"you intend to use every month: ";
+    std::cin>>hours;
     hrsRslt = hours;
     
     if(choice=='a'||choice=='A'){
@@ -144,9 +144,9 @@ void prob2(){
     }
     
     //display results
-    cout<<"Plan: "<<choice<<endl;
-    cout<<"Hours: "<<hrsRslt<<endl;
-    cout<<"Monthly Charge: $"<<total<<".00/month"<<endl;
+    std::cout<<"Plan: "<<choice<<std::endl;
+    std::cout<<"Hours: "<<hrsRslt<<std::endl;
+    std::cout<<"Monthly Charge: $"<<total<<".00/month"<<std::endl;
 }
 void prob3(){
     
@@ -155,8 +155,8 @@ void prob3(){
     int r0=0,r1=1,r2; 
     
     //prompt
-    cout<<"Input number in Fibonacci sequence to find:";
-    cin>>num;
+    std::cout<<"Input number in Fibonacci sequence to find:";
+    std::cin>>num;
     
     //calculate
     for(int i=2;i<num;i++){
@@ -171,6 +171,6 @@ void prob3(){
     else fib = r2;
     
     //output
-    cout<<"Input: "<<num<<endl;
-    cout<<"Output: "<<fib<<endl;
+    std::cout<<"Input: "<<num<<std::endl;
+    std::cout<<"Output: "<<fib<<std::endl;
 }
--- Midterm/C++_code/Midterm_C++/nbproject/Makefile-variables.mk
+++ Midterm/C++_code/Midterm_C++/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=Cygwin_4.x_1-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x_1-Windows
 CND_ARTIFACT_NAME_Debug=midterm_c__
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/midterm_c__
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x_1-Windows/midterm_c__
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x_1-Windows/package
 CND_PACKAGE_NAME_Debug=midtermc++.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/midtermc++.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x_1-Windows/package/midtermc++.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=Cygwin_4.x_1-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x_1-Windows
 CND_ARTIFACT_NAME_Release=midterm_c__
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/midterm_c__
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x_1-Windows/midterm_c__
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x_1-Windows/package
 CND_PACKAGE_NAME_Release=midtermc++.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/midtermc++.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x_1-Windows/package/midtermc++.tar
 #
 # include compiler specific variables
 #
